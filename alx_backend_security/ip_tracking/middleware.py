640b5b2394cb81e056f10cdd3ea2f719b1af29b4
Linda Ngunjiri <ciiru.ngunjiri@gmail.com>
2025-09-01 19:50:00 +0300

    add IP blocking model based on a blacklist

--- alx_backend_security/ip_tracking/middleware.py
+++ alx_backend_security/ip_tracking/middleware.py
@@ -1,6 +1,8 @@
 from django.utils.timezone import now
-from .models import RequestLog
-from ipware import get_client_ip  # django-ipware
+from .models import RequestLog, BlockedIP
+from ipware import get_client_ip 
+from django.http import HttpResponseForbidden
+
 
 class IPLoggingMiddleware:
     def __init__(self, get_response):
@@ -25,3 +27,26 @@ class IPLoggingMiddleware:
         )
 
         return response
+
+class IPLoggingAndBlockingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip, is_routable = get_client_ip(request)
+        if ip is None:
+            ip = '0.0.0.0'
+
+        # Check if IP is blocked
+        if BlockedIP.objects.filter(ip_address=ip).exists():
+            return HttpResponseForbidden("Access denied: Your IP is blocked.")
+
+        # Log the IP request as before
+        from django.utils.timezone import now
+        from .models import RequestLog
+        
+        RequestLog.objects.create(ip_address=ip, timestamp=now(), path=request.path)
+
+        response = self.get_response(request)
+        return response
+    
